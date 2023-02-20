eeb9e579300bd214a2437164594ef34f87d86fb9
lblasnar10 <luis.blas.narvaez@gmail.com>
2023-02-20 15:55:19 +0100

    Test for process SQS and SNS

--- src/main/java/org/example/lambda/MyJavaLambdaHandler.java
+++ src/main/java/org/example/lambda/MyJavaLambdaHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.example.utils.LambdaUtils;
 
 import java.io.Serializable;
 
@@ -15,7 +16,9 @@ import java.io.Serializable;
 public class MyJavaLambdaHandler implements RequestHandler<Serializable, String> {
     @Override
     public String handleRequest(Serializable input, Context context) {
+        var logger = LambdaUtils.initializeLogger(context);
         String eventType;
+        logger.log("Message received from type" + input.getClass());
         if (input instanceof SNSEvent) {
             eventType = new MyLambdaHandlerLab().handleRequest((SNSEvent) input, context);
         } else if (input instanceof KinesisEvent) {
@@ -23,6 +26,7 @@ public class MyJavaLambdaHandler implements RequestHandler<Serializable, String>
         } else if (input instanceof SQSEvent) {
             eventType = new MyLambdaSQSHandlerLab().handleRequest((SQSEvent) input, context);
         } else {
+            logger.log("Not known event type");
             eventType = "Not known event";
         }
         return eventType;
