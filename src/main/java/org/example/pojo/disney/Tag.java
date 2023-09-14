6f592dc0370b88e9c6fdcd508e535a4fc4cdabec
lblasnar10 <luis.blas.narvaez@gmail.com>
2023-09-14 22:37:32 +0200

    test

---
+++ src/main/java/org/example/pojo/disney/Tag.java
@@ -0,0 +1,17 @@
+
+package org.example.pojo.disney;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Value;
+
+@Value
+public class Tag {
+
+    @SerializedName("name")
+    @Expose
+    String name;
+    @SerializedName("data")
+    @Expose
+    String data;
+}
